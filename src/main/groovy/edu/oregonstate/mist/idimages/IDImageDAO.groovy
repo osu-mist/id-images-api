a99ba03acbb80a87fd02f24b171d92cc089c968c
Jared Kosanovic <jared@kosanovj-laptop-ethernet.tss.oregonstate.edu>
2016-08-26 16:44:46 -0700

    Added build step to clone private dao and implemented private dao

--- src/main/groovy/edu/oregonstate/mist/idimages/IDImageDAO.groovy
+++ src/main/groovy/edu/oregonstate/mist/idimages/IDImageDAO.groovy
@@ -4,12 +4,11 @@ import org.skife.jdbi.v2.sqlobject.Bind
 import org.skife.jdbi.v2.sqlobject.SqlQuery
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper
 import java.sql.Blob
+import edu.oregonstate.mist.contrib.AbstractIdImageDao
 
 @RegisterMapper(ImageMapper)
 public interface IDImageDAO extends Closeable {
-    @SqlQuery("""
-       redacted
-        """)
+    @SqlQuery(AbstractIdImageDao.getByID)
     Blob getByID(@Bind("id") Integer id)
 
     @Override
--- src/main/groovy/edu/oregonstate/mist/idimages/ImageMapper.groovy
+++ src/main/groovy/edu/oregonstate/mist/idimages/ImageMapper.groovy
@@ -5,9 +5,10 @@ import org.skife.jdbi.v2.tweak.ResultSetMapper
 import java.sql.Blob
 import java.sql.ResultSet
 import java.sql.SQLException
+import edu.oregonstate.mist.contrib.AbstractIdImageDao
 
 public class ImageMapper implements ResultSetMapper<Blob> {
     public Blob map(int i, ResultSet rs, StatementContext sc) throws SQLException {
-        rs.getBlob("Redacted")
+        rs.getBlob(AbstractIdImageDao.mapperColumnName)
     }
 }
