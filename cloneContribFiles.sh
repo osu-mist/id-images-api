72c06ab61f295dd2b107d6675db359d7b17c340f
Jared Kosanovic <jared@kosanovj-laptop-ethernet.tss.oregonstate.edu>
2016-08-30 13:44:35 -0700

    moved contrib repo to gradle.properties

--- cloneContribFiles.sh
+++ cloneContribFiles.sh
@@ -1,4 +1,4 @@
 rm -rf src/main/groovy/edu/oregonstate/mist/contrib/
-git clone --depth=1 -q git@github.sig.oregonstate.edu:ecs-data/id-images-api-contrib.git src/main/groovy/edu/oregonstate/mist/contrib
+git clone --depth=1 -q $1 src/main/groovy/edu/oregonstate/mist/contrib
 rm -rf src/main/groovy/edu/oregonstate/mist/contrib/.git/
 rm src/main/groovy/edu/oregonstate/mist/contrib/.gitignore
