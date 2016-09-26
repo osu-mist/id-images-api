344246b3c5046fb410a420bf35b2121cb093e5b2
Jared Kosanovic <jared@kosanovj-laptop-ethernet.tss.oregonstate.edu>
2016-09-26 15:31:52 -0700

    added placeholder method and resizing functionality

--- src/main/groovy/edu/oregonstate/mist/idimages/resources/IdImagesResource.groovy
+++ src/main/groovy/edu/oregonstate/mist/idimages/resources/IdImagesResource.groovy
@@ -1,10 +1,13 @@
 package edu.oregonstate.mist.idimages.resources
 
 import com.codahale.metrics.annotation.Timed
+import com.sun.corba.se.spi.orbutil.fsm.Input
 import edu.oregonstate.mist.api.AuthenticatedUser
 import edu.oregonstate.mist.api.Resource
 import edu.oregonstate.mist.idimages.IDImageDAO
 import io.dropwizard.auth.Auth
+import org.yaml.snakeyaml.Yaml
+
 import javax.imageio.ImageIO
 import javax.ws.rs.PathParam
 import javax.ws.rs.core.Response
@@ -12,6 +15,8 @@ import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import java.awt.Graphics2D
+import java.awt.Image
 import java.awt.image.BufferedImage
 import java.sql.Blob
 import org.slf4j.LoggerFactory
@@ -35,21 +40,46 @@ class IdImagesResource extends Resource {
     @Produces("image/jpg")
     @Timed
     public Response getByOSUID(@Auth AuthenticatedUser _, @PathParam('id') String id) {
-        Blob image = idImageDAO.getByID(id)
+        Integer bannerPIDM = idImageDAO.getPIDM(id)
 
-        if (!image) {
+        if (!bannerPIDM) {
             return notFound().type(MediaType.APPLICATION_JSON).build()
         }
+        Blob image = idImageDAO.getByID(id)
+
+        //Return a placeholder image if a person exists but doesn't have an ID image
+        if (!image) {
+            return ok(getImageStream(ImageIO.read(new File("images/defaultImage.jpg")))).build()
+        }
+        //Return an ID image of a person
         try {
-            BufferedImage buffImg = ImageIO.read(image.getBinaryStream())
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
-            ImageIO.write(buffImg, "jpg", outputStream)
-            byte[] imageData = outputStream.toByteArray()
-            Response.ok(imageData).build()
+            Response.ok(getImageStream(ImageIO.read(image.getBinaryStream()))).build()
         } catch (Exception e) {
             logger.error("Exception while calling getIDImages", e)
             return internalServerError("Internal server error").
                     type(MediaType.APPLICATION_JSON).build()
         }
+    }
+    //Resizes a BufferedImage to a width constraint and calculates the height accordingly
+    public static BufferedImage resizeWidthConstraint(BufferedImage img, int newWidth) {
+        Integer newHeight = ((img.getHeight() * newWidth) / img.getWidth())
+
+        Image tmp = img.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH)
+        BufferedImage resizedImg = new BufferedImage(newWidth, newHeight, img.getType())
+
+        Graphics2D g2d = resizedImg.createGraphics()
+        g2d.drawImage(tmp, 0, 0, null)
+        g2d.dispose()
+
+        resizedImg
+    }
+    //Converts a BufferedImage into a format able to returned by the API
+    public def getImageStream(BufferedImage origImg) {
+        BufferedImage resizedImg = resizeWidthConstraint(origImg, 150)
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
+        ImageIO.write(resizedImg, "jpg", outputStream)
+        byte[] imageData = outputStream.toByteArray()
+
+        imageData
     }
 }
